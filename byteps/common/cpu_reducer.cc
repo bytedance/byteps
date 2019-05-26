f0ef7a0b387fcc962328157b6186de30c58d9fb0
Yibo Zhu <zhuyibo@bytedance.com>
2019-05-25 17:26:28 -0700

    common: performance tuning for cpu reducer

--- byteps/common/cpu_reducer.cc
+++ byteps/common/cpu_reducer.cc
@@ -37,102 +37,95 @@ bool CpuReducer::isRoot() {
     return (_comm->getRoot() == BytePSGlobal::GetLocalRank());
 }
 
-int CpuReducer::sum(void* dst, void* src1, void* src2, size_t len, DataType dtype) {
+int CpuReducer::sum(void* dst, void* src, size_t len, DataType dtype) {
     switch (dtype) {
         case BYTEPS_FLOAT32:
-            return _sum_float32(dst, src1, src2, len);
+            return _sum_float32(dst, src, len);
         case BYTEPS_FLOAT64:
-            return _sum_float64(dst, src1, src2, len);
+            return _sum_float64(dst, src, len);
         case BYTEPS_FLOAT16:
-            return _sum_float16(dst, src1, src2, len);
+            return _sum_float16(dst, src, len);
         case BYTEPS_UINT8:
-            return _sum_unit8(dst, src1, src2, len);
+            return _sum_unit8(dst, src, len);
         case BYTEPS_INT32:
-            return _sum_int32(dst, src1, src2, len);
+            return _sum_int32(dst, src, len);
         case BYTEPS_INT8:
-            return _sum_int8(dst, src1, src2, len);
+            return _sum_int8(dst, src, len);
         case BYTEPS_INT64:
-            return _sum_int64(dst, src1, src2, len);
+            return _sum_int64(dst, src, len);
         default:
             BPS_CHECK(0) << "Unsupported data type: " << dtype;
     }
     return 0;
 }
 
-int CpuReducer::_sum_float32(void* dst, void* src1, void* src2, size_t len) {
+int CpuReducer::_sum_float32(void* dst, void* src, size_t len) {
     auto d = (float*)dst;
-    auto s1 = (float*)src1;
-    auto s2 = (float*)src2;
+    auto s = (float*)src;
 #pragma omp parallel for simd num_threads(_num_threads)
-    for (int i = 0; i < len / 4; ++i) {
-        d[i] = s1[i] + s2[i];
+    for (size_t i = 0; i < len / 4; ++i) {
+        d[i] = d[i] + s[i];
     }
     return 0;
 }
 
-int CpuReducer::_sum_float64(void* dst, void* src1, void* src2, size_t len) {
+int CpuReducer::_sum_float64(void* dst, void* src, size_t len) {
     auto d = (double*)dst;
-    auto s1 = (double*)src1;
-    auto s2 = (double*)src2;
+    auto s = (double*)src;
 #pragma omp parallel for simd num_threads(_num_threads)
-    for (int i = 0; i < len / 8; ++i) {
-        d[i] = s1[i] + s2[i];
+    for (size_t i = 0; i < len / 8; ++i) {
+        d[i] = d[i] + s[i];
     }
     return 0;
 }
 
 // TODO
-int CpuReducer::_sum_float16(void* dst, void* src1, void* src2, size_t len) {
+int CpuReducer::_sum_float16(void* dst, void* src, size_t len) {
     auto d = (float*)dst;
-    auto s1 = (float*)src1;
-    auto s2 = (float*)src2;
+    auto s = (float*)src;
 #pragma omp parallel for simd num_threads(_num_threads)
-    for (int i = 0; i < len / 4; ++i) {
-        d[i] = s1[i] + s2[i];
+    for (size_t i = 0; i < len / 4; ++i) {
+        d[i] = d[i] + s[i];
     }
     return 0;
 }
 
-int CpuReducer::_sum_unit8(void* dst, void* src1, void* src2, size_t len) {
+int CpuReducer::_sum_unit8(void* dst, void* src, size_t len) {
     auto d = (unsigned char*)dst;
-    auto s1 = (unsigned char*)src1;
-    auto s2 = (unsigned char*)src2;
+    auto s = (unsigned char*)src;
 #pragma omp parallel for simd num_threads(_num_threads)
-    for (int i = 0; i < len; ++i) {
-        d[i] = s1[i] + s2[i];
+    for (size_t i = 0; i < len; ++i) {
+        d[i] = d[i] + s[i];
     }
     return 0;
 }
 
-int CpuReducer::_sum_int32(void* dst, void* src1, void* src2, size_t len) {
+int CpuReducer::_sum_int32(void* dst, void* src, size_t len) {
     auto d = (int*)dst;
-    auto s1 = (int*)src1;
-    auto s2 = (int*)src2;
+    auto s = (int*)src;
 #pragma omp parallel for simd num_threads(_num_threads)
-    for (int i = 0; i < len / 4; ++i) {
-        d[i] = s1[i] + s2[i];
+    for (size_t i = 0; i < len / 4; ++i) {
+        d[i] = d[i] + s[i];
     }
     return 0;
 }
 
-int CpuReducer::_sum_int8(void* dst, void* src1, void* src2, size_t len) {
+int CpuReducer::_sum_int8(void* dst, void* src, size_t len) {
     auto d = (signed char*)dst;
-    auto s1 = (signed char*)src1;
-    auto s2 = (signed char*)src2;
+    auto s = (signed char*)src;
 #pragma omp parallel for simd num_threads(_num_threads)
-    for (int i = 0; i < len; ++i) {
-        d[i] = s1[i] + s2[i];
+    for (size_t i = 0; i < len; ++i) {
+        d[i] = d[i] + s[i];
     }
     return 0;
 }
 
-int CpuReducer::_sum_int64(void* dst, void* src1, void* src2, size_t len) {
+int CpuReducer::_sum_int64(void* dst, void* src, size_t len) {
     auto d = (long long*)dst;
-    auto s1 = (long long*)src1;
-    auto s2 = (long long*)src2;
+    auto s = (long long*)src;
 #pragma omp parallel for simd num_threads(_num_threads)
-    for (int i = 0; i < len / 8; ++i) {
-        d[i] = s1[i] + s2[i];
+    for (size_t i = 0; i < len / 8; ++i) {
+        d[i] = d[i] + s[i];
     }
     return 0;
 }
