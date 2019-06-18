50077d371acfdf5d7d7d35f0205ab578b375b55a
jiangyimin <jiangyimin@bytedance.com>
2019-06-18 20:39:45 +0800

    common: remove MPI

--- byteps/common/communicator.cc
+++ byteps/common/communicator.cc
@@ -232,53 +232,5 @@ int BytePSCommSocket::broadcastSignal(void* data, int len) {
     return 0;
 }
 
-#ifdef BYTEPS_USE_MPI
-
-void BytePSCommMPI::init(int* rank, int* size, int* local_rank, int* local_size, int* worker_id, BytePSRole* my_role) {
-    BPS_LOG(DEBUG) << "Using Communicator=MPI";
-
-    int provided;
-    MPI_Init_thread(NULL, NULL, MPI_THREAD_MULTIPLE, &provided);
-
-    _comm = (void*)malloc(sizeof(MPI_Comm));
-    MPI_Comm* mpi_comm = (MPI_Comm*)_comm;
-
-    MPI_Comm_dup(MPI_COMM_WORLD, mpi_comm);
-
-    // Get MPI size to determine how many tensors to wait for before reducing.
-    MPI_Comm_rank(*mpi_comm, local_rank);
-    MPI_Comm_size(*mpi_comm, local_size);
-
-    BPS_CHECK(getenv("DMLC_WORKER_ID")) << "error: env DMLC_WORKER_ID not set";
-    BPS_CHECK(getenv("DMLC_NUM_WORKER")) << "error: env DMLC_NUM_WORKER not set";
-    *worker_id = atoi(getenv("DMLC_WORKER_ID"));
-    auto num_worker = atoi(getenv("DMLC_NUM_WORKER"));
-
-    // we assume _local_size (i.e., # GPU) is consistent on all workers
-    *rank = (*local_rank) + worker_id * (*local_size);
-    *size = num_worker * (*local_size);
-
-    _rank = *rank;
-    _size = *size;
-    _local_rank = *local_rank;
-    _local_size = *local_size;
-
-    return;
-}
-
-int BytePSCommMPI::sendSignal(int destination, void* data, int len) {
-    return 0;
-}
-
-int BytePSCommMPI::recvSignal(int* source, void* data, int max_len) {
-    return 0;
-}
-
-int BytePSCommMPI::broadcastSignal(void* data, int len) {
-    return 0;
-}
-
-#endif // BYTEPS_USE_MPI
-
 }
 }
