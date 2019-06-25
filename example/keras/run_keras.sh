661938be01adac9d223a2e3a0255d186a4dd43b6
jiangyimin <jiangyimin@bytedance.com>
2019-06-25 18:57:58 +0800

    example & test: reformat

--- example/keras/run_keras.sh
+++ example/keras/run_keras.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+path="`dirname $0`"
+
 if [ "$EVAL_TYPE" == "imagenet" ]; then
   echo "Run Keras ImageNet ..."
-  python /opt/tiger/byteps/example/keras/keras_imagenet_resnet50.py $@
+  python $path/keras_imagenet_resnet50.py $@
 elif [ "$EVAL_TYPE" == "mnist" ]; then
   echo "Run Keras MNIST ..."
-  python /opt/tiger/byteps/example/keras/keras_mnist.py $@
+  python $path/keras_mnist.py $@
 elif [ "$EVAL_TYPE" == "mnist_advanced" ]; then
   echo "Run Keras MNIST-advanced ..."
-  python /opt/tiger/byteps/example/keras/keras_mnist_advanced.py $@
+  python $path/keras_mnist_advanced.py $@
 else
   echo "Error: unsupported $EVAL_TYPE"
   exit 1
--- example/pytorch/start_pytorch_byteps.sh
+++ example/pytorch/start_pytorch_byteps.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+path="`dirname $0`"
+
 if [ "$EVAL_TYPE" == "mnist" ]; then
     echo "training mnist..."
-    python /opt/tiger/byteps/example/pytorch/train_mnist_byteps.py $@
+    python $path/train_mnist_byteps.py $@
 elif [ "$EVAL_TYPE" == "imagenet" ]; then
     echo "training imagenet..."
-    python /opt/tiger/byteps/example/pytorch/train_imagenet_resnet50_byteps.py --log-dir $@
+    python $path/train_imagenet_resnet50_byteps.py $@
 elif [ "$EVAL_TYPE" == "benchmark" ]; then
     echo "running benchmark..."
-    python /opt/tiger/byteps/example/pytorch/benchmark_byteps.py $@
+    python $path/benchmark_byteps.py $@
 elif [ "$EVAL_TYPE" == "microbenchmark" ]; then
     echo "running microbenchmark"
-    python /opt/tiger/byteps/example/pytorch/microbenchmark-byteps.py $@
+    python $path/microbenchmark-byteps.py $@
 else
   echo "Error: unsupported $EVAL_TYPE"
   exit 1
