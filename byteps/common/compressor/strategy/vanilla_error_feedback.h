c2f44f6b4f8711c9a074928a667695315b61e9c1
zhongyuchen <izhongyuchen@gmail.com>
2020-01-08 11:38:12 +0000

    compression: fix vtable undefined

--- byteps/common/compressor/strategy/vanilla_error_feedback.h
+++ byteps/common/compressor/strategy/vanilla_error_feedback.h
@@ -29,7 +29,7 @@ class VanillaErrorFeedbackCompressor : public ErrorFeedback {
  public:
   explicit VanillaErrorFeedbackCompressor(
       std::unique_ptr<BaseCompressor> compressor_ptr);
-  ~VanillaErrorFeedbackCompressor();
+  virtual ~VanillaErrorFeedbackCompressor();
 
  protected:
   ByteBuf UpdateGradient(const ByteBuf& grad) override;
