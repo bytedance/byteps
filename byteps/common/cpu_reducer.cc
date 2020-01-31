181641284159bdc4bf98f0c7b99a4f32aca15f1a
zhongyuchen <izhongyuchen@gmail.com>
2020-01-31 20:16:36 +0800

    compression: add sign operator

--- byteps/common/cpu_reducer.cc
+++ byteps/common/cpu_reducer.cc
@@ -222,5 +222,25 @@ int CpuReducer::copy(void* dst, void* src, size_t len) {
 }
 
 
+int CpuReducer::sign(void* dst, void* src, size_t len, DataType dtype) {
+  switch (dtype) {
+    case BYTEPS_FLOAT32:
+      return _sign(reinterpret_cast<float*>(dst), reinterpret_cast<float*>(src),
+                   len);
+    case BYTEPS_FLOAT64:
+      return _sign(reinterpret_cast<double*>(dst),
+                   reinterpret_cast<double*>(src), len);
+    case BYTEPS_FLOAT16:
+      return _sign_float16(dst, src, len);
+    default:
+      BPS_CHECK(0) << "Unsupported data type: " << dtype;
+  }
+}
+
+template <typename T>
+int CpuReducer::_sign(T* dst, T* src, size_t len) {}
+
+int _sign_float16(void* dst, void* src, size_t len) {}
+
 }  // namespace common
 }  // namespace byteps
--- byteps/common/cpu_reducer.h
+++ byteps/common/cpu_reducer.h
@@ -48,6 +48,7 @@ class CpuReducer {
   int sum(void* dst, void* src, size_t len, DataType dtype);
   int sum(void* dst, void* src1, void* src2, size_t len, DataType dtype);
   int copy(void* dst, void* src, size_t len);
+  int sign(void* dst, void* src, size_t len, DataType dtype);
 
 #ifndef BYTEPS_BUILDING_SERVER
   bool isRoot();
@@ -182,6 +183,11 @@ class CpuReducer {
 
   int _sum_float16(void* dst, void* src, size_t len);
   int _sum_float16(void* dst, void* src1, void* src2, size_t len);
+
+  template <typename T>
+  int _sign(T* dst, T* src, size_t len);
+
+  int _sign_float16(void* dst, void* src, size_t len);
 
   float _convert_half_to_full_precision(uint16_t h);
   uint16_t _convert_full_to_half_precision(float f);
