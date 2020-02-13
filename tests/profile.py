968a905cd794f23b729edd8c247ba288e94a3699
zhongyuchen <izhongyuchen@gmail.com>
2020-02-13 16:10:28 +0800

    compression: add profile

---
+++ tests/profile.py
@@ -0,0 +1,19 @@
+import mxnet as mx
+
+import byteps.mxnet as bps
+
+bps.init()
+
+mx.random.seed(2020)
+shapes = [2<<x for x in range(24)]
+
+ctx = mx.gpu(0)
+for i, shape in enumerate(shapes):
+    tensor = mx.nd.random.uniform(-100, 100, shape=shape, ctx=ctx)
+    setattr(tensor, "byteps_compressor_type", "onebit")
+    byteps_params = dict(
+        filter(lambda attr: attr[0].startswith(
+            "byteps_",), param.__dict__.items())
+    )
+    bps.byteps_declare_tensor("gradient_" + str(i), **byteps_params)
+    bps.byteps_push_pull(tensor, name="gradient_" + str(i))
