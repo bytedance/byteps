7e6b37edebb92b5b31079ac45fa09c5eebee8116
zhongyuchen <izhongyuchen@gmail.com>
2020-02-20 10:32:47 +0800

    fix typo

--- setup.py
+++ setup.py
@@ -169,7 +169,7 @@ def get_mpi_flags():
 
 def get_cpp_flags(build_ext):
     last_err = None
-    default_flags = ['-std=c++11', '-fPIC', '-Ofast', '-Wall', '-fopenmp', '-match=native']
+    default_flags = ['-std=c++11', '-fPIC', '-Ofast', '-Wall', '-fopenmp', '-march=native']
     flags_to_try = []
     if sys.platform == 'darwin':
         # Darwin most likely will have Clang, which has libc++.
