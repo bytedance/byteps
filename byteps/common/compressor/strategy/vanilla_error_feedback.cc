2c97ce5bd9fcfa1e7ddcef5ecda059cfb4426641
zhongyuchen <izhongyuchen@gmail.com>
2020-02-25 23:33:30 +0800

    compression: fix ef bug

--- byteps/common/compressor/strategy/vanilla_error_feedback.cc
+++ byteps/common/compressor/strategy/vanilla_error_feedback.cc
@@ -47,8 +47,11 @@ void VanillaErrorFeedbackCompressor::UpdateGradient(ByteBuf grad, int dtype) {
 
 void VanillaErrorFeedbackCompressor::UpdateError(ByteBuf corrected, int dtype,
                                                  ByteBuf* compressed) {
-  ByteBuf decompressed{_decode_buf.get(), corrected.size};
-  Decompress(*compressed, dtype, &decompressed);
+  // TODO: we may remove this copy in the futher
+  this->_cpu_reducer->copy(_error.get(), compressed->data, compressed->size);
+  ByteBuf compressed_copy{_error.get(), compressed->size},
+      decompressed{_debuf.get(), corrected.size};
+  Decompress(compressed_copy, dtype, &decompressed);
   this->_cpu_reducer->sum(_error.get(), corrected.data, decompressed.data,
                           corrected.size, static_cast<DataType>(dtype), -1.0);
 }
