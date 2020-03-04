b6fea7ebdef33a082a001284a4085ee66756c7f0
zhongyuchen <izhongyuchen@gmail.com>
2020-03-04 22:56:54 +0800

    compression: test fp16 fp64

--- tests/compressor/test_meta.py
+++ tests/compressor/test_meta.py
@@ -16,8 +16,7 @@
 
 class TestMeta(type):
     def __new__(mcls, name, bases, dct):
-        test_dtypes = ['float32']
-        # test_dtypes = ['float32', 'float16', 'float64'] 
+        test_dtypes = ['float32', 'float16', 'float64'] 
 
         def gen_test(dtype):
             def test(self):
