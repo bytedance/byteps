cb65d2126954bac6045e67064f1c56b614d94694
zhongyuchen <izhongyuchen@gmail.com>
2020-03-16 18:40:09 +0800

    compression: fix typo

--- byteps/mxnet/ops.h
+++ byteps/mxnet/ops.h
@@ -38,8 +38,8 @@ extern "C" int byteps_mxnet_push_pull_async(NDArray* input, char* name,
                                             bool is_average);
 
 extern "C" void byteps_mxnet_declare_tensor(char* name, int num_args,
-                                            char** args_keys,
-                                            char** args_vals);
+                                            const char** args_keys,
+                                            const char** args_vals);
 
 }  // namespace mxnet
 }  // namespace byteps
