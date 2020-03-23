7d6d6388de40260528a42a0e736f41704dfed789
zhongyuchen <izhongyuchen@gmail.com>
2020-03-23 15:03:11 +0800

    async: add func loops

--- byteps/common/operations.cc
+++ byteps/common/operations.cc
@@ -47,6 +47,7 @@ void byteps_lazy_init() {
   if (BytePSGlobal::IsDistributed()) {
     if (BytePSGlobal::IsRootDevice()) {
       func.push_back(PullLoop);
+      func.push_back(DecompressLoop);
     }
   }
 
@@ -62,6 +63,7 @@ void byteps_lazy_init() {
       // PUSH can be a real push in distributed mode
       // Or a dummy barrier in cross-pcie-switch mode
       func.push_back(PushLoop);
+      func.push_back(CompressLoop);
       func.push_back(RootCopyHost2DeviceLoop);
     } else {
       func.push_back(CoordinatePushLoop);
