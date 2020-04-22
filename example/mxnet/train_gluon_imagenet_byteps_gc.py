51ab4adb1d985f8009cff0c15597dc9b12002648
zhongyuchen <izhongyuchen@gmail.com>
2020-04-22 14:13:50 +0800

    script: update

--- example/mxnet/train_gluon_imagenet_byteps_gc.py
+++ example/mxnet/train_gluon_imagenet_byteps_gc.py
@@ -166,7 +166,7 @@ def main():
     num_batches = num_training_samples // (batch_size*nworker)
 
     lr_scheduler = LRSequential([
-        LRScheduler('linear', base_lr=0, target_lr=opt.lr * nworker,
+        LRScheduler('linear', base_lr=opt.warmup_lr, target_lr=opt.lr * nworker,
                     nepochs=opt.warmup_epochs, iters_per_epoch=num_batches),
         LRScheduler(opt.lr_mode, base_lr=opt.lr * nworker, target_lr=0,
                     nepochs=opt.num_epochs - opt.warmup_epochs,
