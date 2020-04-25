863441cee68c13dc91e8ad52d8cc6246551f16fb
zhongyuchen <izhongyuchen@gmail.com>
2020-04-25 21:36:49 +0800

    ci: fix typos

--- launcher/launch.py
+++ launcher/launch.py
@@ -16,13 +16,7 @@ class PropagatingThread(threading.Thread):
     def run(self):
         self.exc = None
         try:
-            if hasattr(self, '_Thread__target'):
-                #  python 2.x
-                self.ret = self._Thread__target(
-                    *self._Thread__args, **self._Thread__kwargs)
-            else:
-                # python 3.x
-                self.ret = self._target(**self._args, **self._kwargs)
+            self.ret = self._target(*self._args, **self._kwargs)
         except BaseException as e:
             self.exc = e
 
