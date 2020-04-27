7ba38db47412fd79c9e30f28bc9692c11c0bddae
zhongyuchen <izhongyuchen@gmail.com>
2020-04-27 11:32:30 +0800

    ci: update

--- tests/run_byteps_test.sh
+++ tests/run_byteps_test.sh
@@ -2,20 +2,23 @@
 
 path="`dirname $0`"
 
-echo "Launch scheduler"
-sh $path/run_byteps_server_local.sh scheduler 
-
-echo "Launch server"
-sh $path/run_byteps_server_local.sh server
-
 export PATH=~/.local/bin:$PATH
-export NVIDIA_VISIBLE_DEVICES=0
 export DMLC_NUM_WORKER=1
-export DMLC_WORKER_ID=0
-export DMLC_ROLE=worker
 export DMLC_NUM_SERVER=1
 export DMLC_PS_ROOT_URI=127.0.0.1
 export DMLC_PS_ROOT_PORT=1234
+
+echo "Launch scheduler"
+export DMLC_ROLE=scheduler
+bpslaunch &
+
+echo "Launch server"
+export DMLC_ROLE=server
+bpslaunch &
+
+export NVIDIA_VISIBLE_DEVICES=0
+export DMLC_WORKER_ID=0
+export DMLC_ROLE=worker
 export BYTEPS_FORCE_DISTRIBUTED=1
 
 if [ "$TEST_TYPE" == "mxnet" ]; then
