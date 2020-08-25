7245acc8ad6bc72e95f7d3c6bba11c25718a81d5
zhongyuchen <izhongyuchen@gmail.com>
2020-08-25 05:48:04 +0000

    randomk change check to le

--- byteps/common/compressor/impl/randomk.cc
+++ byteps/common/compressor/impl/randomk.cc
@@ -80,10 +80,8 @@ tensor_t RandomkCompressor::CompressImpl(index_t* dst, const scalar_t* src,
 
 #else
   // for servers
-  // should be exactly be k
-  BPS_LOG(INFO) << "_non_zero_idx.size()=" << _non_zero_idx.size()
-                << "\t k=" << this->_k;
-  BPS_CHECK(_non_zero_idx.size() == this->_k);
+  // should be not greater than k
+  BPS_CHECK_LE(_non_zero_idx.size(), this->_k);
   size_t i = 0;
   for (auto& index : _non_zero_idx) {
     ptr[i++] = std::make_pair(index, src[index]);
