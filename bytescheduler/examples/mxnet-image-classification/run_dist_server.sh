989ad64af8643df76ee2886117dcb20269ec4216
rivendile <908636821@qq.com>
2020-10-27 09:40:49 +0000

    pulll

--- bytescheduler/examples/mxnet-image-classification/run_dist_server.sh
+++ bytescheduler/examples/mxnet-image-classification/run_dist_server.sh
@@ -1,1 +1,1 @@
-./dist_server.sh 1 1  python train_imagenet.py --network vgg --num-layers 16 --benchmark 1 --kv-store dist_sync --batch-size 32 --disp-batches 50 --num-examples 5120 --num-epochs 1 --gpus 0
+./dist_server.sh 1 1  python train_imagenet.py --network vgg --num-layers 16 --benchmark 1 --kv-store dist_sync --batch-size 32 --disp-batches 10 --num-examples 1920 --num-epochs 1 --gpus 0
