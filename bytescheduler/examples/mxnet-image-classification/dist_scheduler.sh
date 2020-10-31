35b26a6f2f1133abc8f5ddb28a562ea3942cda2f
rivendile <908636821@qq.com>
2020-10-31 02:55:23 +0000

    push

--- bytescheduler/examples/mxnet-image-classification/dist_scheduler.sh
+++ bytescheduler/examples/mxnet-image-classification/dist_scheduler.sh
@@ -24,7 +24,7 @@ shift
 arg="$@"
 
 # start the scheduler
-export DMLC_PS_ROOT_URI='172.31.95.96'
+export DMLC_PS_ROOT_URI='172.31.92.208'
 export DMLC_PS_ROOT_PORT=8000
 export DMLC_ROLE='scheduler'
 MXNET_PROFILER_AUTOSTART=1 ${bin} ${arg} &
