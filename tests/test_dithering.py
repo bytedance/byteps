30c9162adefaa18e5f5593c750813953fd673563
zhongyuchen <izhongyuchen@gmail.com>
2020-11-03 23:40:11 +0800

    fix typo

--- tests/test_dithering.py
+++ tests/test_dithering.py
@@ -52,7 +52,7 @@ def dithering(x, k, state, partition='linear', norm="max"):
         scale = np.linalg.norm(y, ord=2)
     else:
         raise ValueError("Unsupported normalization")
-    sign = (0 < y) - (y - 0)
+    sign = (0 < y) - (y < 0)
     sign = sign.astype(np.int32)
     y = np.abs(y)
     y /= scale
@@ -78,7 +78,7 @@ def dithering(x, k, state, partition='linear', norm="max"):
     else:
         raise ValueError("Unsupported partition")
 
-    y *= (1 - (sign << 1))
+    y *= sign
     return y.reshape(x.shape).astype(dtype)
 
 
