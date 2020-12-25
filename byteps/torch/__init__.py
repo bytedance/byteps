9dbe060fa03b3ed84c48d4958f0355f12e6a5e67
zhongyuchen <izhongyuchen@gmail.com>
2020-12-25 12:11:16 +0800

    restore

--- byteps/torch/__init__.py
+++ byteps/torch/__init__.py
@@ -111,15 +111,11 @@ class _DistributedOptimizer(torch.optim.Optimizer):
                         name = self._parameter_names.get(param.__hash__())
                     else:
                         name = self._parameter_names.get(param)
-                    # TODO
-                    if "embeddings" in name:
-                        declare("Gradient."+name)
-                    else:
-                        byteps_params = dict(
-                            filter(lambda attr: attr[0].startswith(
-                                "byteps_",), param.__dict__.items())
-                        )
-                        declare("Gradient."+name, **byteps_params)
+                    byteps_params = dict(
+                        filter(lambda attr: attr[0].startswith(
+                            "byteps_",), param.__dict__.items())
+                    )
+                    declare("Gradient."+name, **byteps_params)
                     print("Graient."+name, param.data.numel())
 
     @ staticmethod
