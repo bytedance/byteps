torch: fix broadcast_optimizer_state when used with torch 1.8.0
fix the following error in broadcast_optimizer_state when working with
torch 1.8.0:

  Traceback (most recent call last):
    File "/home/code/example/pytorch/benchmark_byteps.py", line 72, in <module>
      bps.broadcast_optimizer_state(optimizer, root_rank=0)
    File "/root/.local/lib/python3.7/site-packages/byteps-0.7.2-py3.7-linux-x86_64.egg/byteps/torch/__init__.py", line 403, in broadcast_optimizer_state
      p = torch.Tensor([p]).cuda()
  TypeError: must be real number, not NoneType

Signed-off-by: yulu.jia <yulu.jia@bytedance.com>

byteps/torch/__init__.py
@@ -33,6 +33,8 @@ from byteps.torch.ops import send_async, recv_async
 import os
 import torch
 import collections
+import io
+
 
 class _DistributedOptimizer(torch.optim.Optimizer):
     def __init__(self, params, named_parameters, compression,
@@ -516,6 +518,7 @@ def broadcast_optimizer_state(optimizer, root_rank, prefix="Parameter."):
         return
 
     params = []
+    scalars = {}
     callbacks = {}
     occurrences = collections.defaultdict(int)
 
@@ -536,19 +539,21 @@ def broadcast_optimizer_state(optimizer, root_rank, prefix="Parameter."):
             return dtype(x)
 
     # Some optimizer parameters may be represented as scalars instead of
-    # tensors.  In such cases, we need to wrap the scalar in a tensor, then
-    # broadcast, then update the appropriate value in the state_dict with the
-    # new unwrapped scalar value via a callback.
-    def _create_callback(pid, name, t, p):
-        def _from_tensor():
-            state_dict['state'][pid][name] = t(p.cpu().numpy()[0])
-        return _from_tensor
+    # tensors.  In such cases, we place the scalars into a single dict,
+    # then pickle and broadcast with broadcast_object (under the assumption
+    # that there are not many scalars, and so the overhead of pickling will
+    # be relatively low). Because broadcast_object is performed out-of-place,
+    # we then use a callback to assign the new value to the correct element
+    # of the optimizer state.
+    def _create_state_callback(pid, name):
+        def _assign_state(v):
+            state_dict['state'][pid][name] = v
+        return _assign_state
 
-    def _create_option_callback(index, option_key, option_tensor, dtypes):
-        def _from_tensor():
-            optimizer.param_groups[index][option_key] = _recursive_cast(
-                option_tensor.cpu().numpy()[0], dtypes)
-        return _from_tensor
+    def _create_option_callback(index, option_key):
+        def _assign_option(v):
+            optimizer.param_groups[index][option_key] = v
+        return _assign_option
 
     # Param groups are an ordered list, normally there is only one per model,
     # but users can add additional param groups for example to train
@@ -559,19 +564,19 @@ def broadcast_optimizer_state(optimizer, root_rank, prefix="Parameter."):
             if option_key == 'params':
                 continue
 
-            # Options like the learning rate are scalar, and need to be wrapped in tensors
+            # Options like the learning rate are scalar, and need to be broadcast separately
             key = '%s.%d' % (option_key, index)
             dtypes = _get_types(option_value)
             option_tensor = torch.Tensor([option_value]).cuda()
-            callbacks[key] = _create_option_callback(index, option_key, option_tensor, dtypes)
-            params.append((key, option_tensor))
+            scalars[key] = option_value
+            callbacks[key] = _create_option_callback(index, option_key)
 
         # The params list here is ordered by the layers in the model
-        
         for pid in group['params']:
             if pid not in state_dict['state']:
                 # The param has not set requires_grad, so skip broadcast
                 continue
+
             param_state = state_dict['state'][pid]
             for name, p in param_state.items():
                 # Some parameter names may appear more than once, in which
@@ -580,19 +585,62 @@ def broadcast_optimizer_state(optimizer, root_rank, prefix="Parameter."):
                 occurrences[name] += 1
                 key = '%s.%d' % (str(name), occurrences[name])
 
-                if not torch.is_tensor(p):
-                    # Wrap the scalar in a FloatTensor, and remember its type
-                    # so we can cast it back after unwrapping
-                    t = type(p)
-                    p = torch.Tensor([p]).cuda()
-                    callbacks[key] = _create_callback(pid, name, t, p)
-
-                params.append((key, p))
+                if torch.is_tensor(p):
+                    # Tensor -> use broadcast_parameters
+                    params.append((key, p))
+                else:
+                    # Scalar -> use broadcast_object
+                    scalars[key] = p
+                    callbacks[key] = _create_state_callback(pid, name)
 
     # Synchronized broadcast of all parameters
     broadcast_parameters(params, root_rank, prefix)
 
-    # Post-broadcast clenaup for non-tensor parameters
-    for key, p in params:
-        if key in callbacks:
-            callbacks[key]()
+    # Broadcast and cleanup for non-tensor parameters
+    scalars = broadcast_object(scalars, root_rank)
+    for key, p in scalars.items():
+        callbacks[key](p)
+
+def broadcast_object(obj, root_rank=0, name=None):
+    """
+    Serializes and broadcasts an object from root rank to all other processes.
+    Typical usage is to broadcast the `optimizer.state_dict()`, for example:
+
+    .. code-block:: python
+
+        state_dict = broadcast_object(optimizer.state_dict(), 0)
+        if bps.rank() > 0:
+            optimizer.load_state_dict(state_dict)
+
+    Arguments:
+        obj: An object capable of being serialized without losing any context.
+        root_rank: The rank of the process from which parameters will be
+                   broadcasted to all other processes.
+        name: Optional name to use during broadcast, will default to the class
+              type.
+    Returns:
+        The object that was broadcast from the `root_rank`.
+    """
+    import cloudpickle
+
+    if name is None:
+        name = type(obj).__name__
+
+    if rank() == root_rank:
+        b = io.BytesIO()
+        cloudpickle.dump(obj, b)
+        t = torch.ByteTensor(bytearray(b.getvalue()))
+        sz = torch.IntTensor([t.shape[0]])
+        broadcast_parameters([(name + '.sz', sz)], root_rank, prefix="Size.")
+    else:
+        sz = torch.IntTensor([0])
+        broadcast_parameters([(name + '.sz', sz)], root_rank, prefix="Size.")
+        t = torch.ByteTensor(sz.tolist()[0])
+
+    broadcast_parameters([(name + '.t', t)], root_rank, prefix="Parameter.")
+
+    if rank() != root_rank:
+        buf = io.BytesIO(t.numpy().tobytes())
+        obj = cloudpickle.load(buf)
+
+    return obj