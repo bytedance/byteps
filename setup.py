9f2f5c32a07c3e83245dcb789815b132ab4a3adf
Yimin Jiang <jiangyimin.jym@bytedance.com>
2022-03-04 12:50:07 +0800

    version: bump up to v0.3.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ URL = 'https://github.com/bytedance/byteps'
 EMAIL = 'lab-hr@bytedance.com'
 AUTHOR = 'Bytedance Inc.'
 REQUIRES_PYTHON = '>=2.7.0'
-VERSION = '0.7.2'
+VERSION = '0.3.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
